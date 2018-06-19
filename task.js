58a26f26cfbdbc6688d0168a159b478744908371
unknown <natdanai.prs@gmail.com>
2018-06-19 14:55:30 +0700

    complete

--- task.js
+++ task.js
@@ -7,14 +7,14 @@ const rl = readline.createInterface({
 
 var task = require('fs');
 const all_task = [];
-var i = 0;
 
-console.log("type 'show' to display all task, type 'del_[number] to delete task example del_1'")
+console.log("type 'show' to display all task, type 'del_[number] to remove task. Example del_1'")
 console.log("please enter your task :");
 
 rl.on('line', (input) => {
     const stop = /^[\s]/;
     const del = /del_[\d]/;
+
     if (input == "" || stop.test(input)) {
         console.log("good bye");
         rl.close();
@@ -23,23 +23,30 @@ rl.on('line', (input) => {
         show_all_task()
     }
     else if (del.test(input)) {
-        let num = input.slice(input.indexOf("_"),input.length)
+        let num = input.slice(input.indexOf("_")+1,input.length)
         delete_task(num);
     }
     else {
         console.log(`Received: ${input}`);
-        all_task[i] = input;
-        i = all_task.length;
+        all_task.push(input);
     }
 });
 
 function show_all_task() {
-    for(var j in all_task) {
-        console.log(`${j}. ${all_task[j]}`);
+    var j = 0;
+    console.log(all_task)
+    for(j; j<=all_task.length; j++) {
+        if(all_task[j] != undefined){
+            console.log(`${j+1}. ${all_task[j]}`);
+        }
+        else{
+            
+        }
     }
 }
 
 function delete_task(num) {
+    num = num - 1;
     if(all_task[num] == undefined){
         console.log("undefined task");
     }
